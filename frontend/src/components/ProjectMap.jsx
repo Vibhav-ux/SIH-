87335dc8298a1ad7707309f708abf9acf0f4a362
Vaibhav029coder <vaibhavpandey9621@gmail.com>
2026-09-17 23:32:35 +0530

    style: change global map base layer to Esri World Imagery satellite view

--- frontend/src/components/ProjectMap.jsx
+++ frontend/src/components/ProjectMap.jsx
@@ -36,9 +36,8 @@ export default function ProjectMap({ projects = [], height = 500, onMarkerClick 
       zoomControl: true,
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      attribution: '© OpenStreetMap © CARTO',
-      subdomains: 'abcd',
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
       maxZoom: 18,
     }).addTo(mapInstanceRef.current);
 
